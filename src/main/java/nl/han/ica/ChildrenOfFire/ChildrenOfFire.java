950fedfab88568dd89fe397f13996599b4507a3d
Zatura24 <jannick.joosten@gmail.com>
2018-03-30 12:57:21 +0200

    Added default file for the game

---
+++ src/main/java/nl/han/ica/ChildrenOfFire/ChildrenOfFire.java
@@ -0,0 +1,30 @@
+package nl.han.ica.ChildrenOfFire;
+
+import nl.han.ica.OOPDProcessingEngineHAN.Engine.GameEngine;
+import processing.core.PApplet;
+
+/**
+* ChildrenOfFire
+*/
+@SuppressWarnings("serial")
+public class ChildrenOfFire extends GameEngine {
+    public static void main(String[] args) {
+        PApplet.main(new String[] { "nl.han.ica.ChildrenOfFire.ChildrenOfFire" });
+    }
+
+    /**
+     * setup of the whole game
+     */
+    @Override
+    public void setupGame() {
+
+    }
+
+    /**
+     * this function will be called every frame;
+     */
+    @Override
+    public void update() {
+
+    }
+}
